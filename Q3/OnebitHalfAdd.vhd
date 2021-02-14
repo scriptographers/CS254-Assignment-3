7678382cda6761ab919f2f442b6a086fd043214b
devansh-dvj <devansh.dvj@gmail.com>
2021-02-14 08:22:48 +0530

    Q3: Added base

---
+++ Q3/OnebitHalfAdd.vhd
@@ -0,0 +1,23 @@
+-- TOP MODULE OnebitHalfAdd
+library work;
+use work.all;
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity OnebitHalfAdd is
+   port (a, b : in std_logic;
+         sum, carry: out std_logic);
+end entity;
+
+architecture HalfAdd of OnebitHalfAdd is
+
+   component TwoByOneMux is
+      port (i : in std_logic_vector(1 downto 0);
+            sel : in std_logic;
+            z : out std_logic);
+   end component;
+
+begin
+
+end architecture;
